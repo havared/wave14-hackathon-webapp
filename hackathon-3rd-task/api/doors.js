7e6875120a573d267d24246e3c72de28617a840f
shaktiram <shakthi16277@gmail.com>
2017-01-12 15:05:31 +0530

    updated

--- hackathon-3rd-task/api/doors.js
+++ hackathon-3rd-task/api/doors.js
@@ -1,15 +1,29 @@
 const fs = require('fs')
 
-exports.insertData = function(req,res){
+module.exports.insertData = function(req,res){
   var jsonObj = req.body;
   var fileObj = null;
-  let data = fs.readFileSync("./data/file1.json").toString();
-  fileJsonObj = JSON.parse(data);
-  for(let each in jsonObj){
-    fileJsonObj['doors'][0][each] = jsonObj[each];
-  }
-  fs.writeFile('./data/file1.json', JSON.stringify(fileJsonObj,null,2),'utf-8');
-  res.end('done');
+  var greenJsonObj = null;
+  
+   fs.readFile("./data/file1.json", 'utf-8',function (err, data) {
+   if(err){
+     res.end('Error');
+   }
+   else
+   {
+     greenJsonObj = JSON.parse(data);
+     greenJsonObj['doors'][greenJsonObj['doors'].length] = jsonObj;
+     fs.writeFile('./data/file1.json', JSON.stringify(greenJsonObj,null,2),'utf-8', function(error){
+       if(error){
+         res.end('Error');
+       }
+       else
+       {
+         res.end('File has been written');
+       }
+     });
+   }
+ });
 }
 
 findGreen = function(id){
@@ -44,7 +58,7 @@ findBlack = function(id){
   })
 }
 
-exports.find = function(req,res){
+module.exports.find = function(req,res){
   var allPromises = [findGreen(req.params.id), findBlack(req.params.id)];
   Promise.all(allPromises).then(function(data){
     res.json(data);
